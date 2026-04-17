d1ff13b80e2280402594f38847114a869f95a8ff
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2026-04-17 10:23:45 +0800

    Done with creating a native stack navigator section

---
+++ react-native-navigation/screens/Home.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
+}
