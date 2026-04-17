4172fdbca402c695c251599e3d1c87f9cfa1e323
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2026-04-17 11:07:36 +0800

    Done with using the navigation object section

--- react-native-navigation/screens/DetailsScreen.js
+++ react-native-navigation/screens/DetailsScreen.js
@@ -1,15 +1,25 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import { createStaticNavigation } from '@react-navigation/native';
+import {
+  createStaticNavigation,
+  useNavigation,
+} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from '@react-navigation/elements';
 
 function HomeScreen() {
+  const navigation = useNavigation();
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Details')}>
+        Go to Details
+      </Button>
     </View>
   );
 }
+
 
 export function DetailsScreen() {
   return (
