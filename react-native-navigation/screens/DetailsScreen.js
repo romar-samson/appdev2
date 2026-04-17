7c91ac961c1d96eb00cc6bd5a42d774edf7c7eeb
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2026-04-17 11:12:42 +0800

    Done with going back section

--- react-native-navigation/screens/DetailsScreen.js
+++ react-native-navigation/screens/DetailsScreen.js
@@ -20,11 +20,20 @@ function HomeScreen() {
   );
 }
 
+function DetailsScreen() {
+  const navigation = useNavigation();
 
-export function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button onPress={() => navigation.push('Details')}>
+        Go to Details... again
+      </Button>
+      <Button onPress={() => navigation.goBack()}>Go back</Button>
+      <Button onPress={() => navigation.popTo('Home')}>Go to Home</Button>
+      <Button onPress={() => navigation.popToTop()}>
+        Go back to first screen in stack
+      </Button>
     </View>
   );
 }
