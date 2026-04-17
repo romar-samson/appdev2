087412057f8abc393a1792618c27a8f0fab6722c
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2026-04-17 10:28:02 +0800

    Done with configuring the navigator section

--- react-native-navigation/index.js
+++ react-native-navigation/index.js
@@ -2,8 +2,9 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 import Home from './screens/Home';
+import Details from './screens/Details';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Home);
+registerRootComponent(Details);
---
+++ react-native-navigation/screens/Details.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
+  screens: {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
+}
